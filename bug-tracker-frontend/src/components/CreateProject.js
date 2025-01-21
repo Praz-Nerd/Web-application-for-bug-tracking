ca26554c14c42c10f42b54cb633f13c2b4570929
UciuIanis <uciuianis22@stud.ase.ro>
2025-01-21 19:47:55 +0200

    implemented create project functionality

--- bug-tracker-frontend/src/components/CreateProject.js
+++ bug-tracker-frontend/src/components/CreateProject.js
@@ -37,8 +37,8 @@ const CreateProject = () => {
       }))
   
       for (let contributor of selectedContributors) {
-        await getResponse(`http://localhost:8080/${user.id}/projects/${project.id}/add-member`, 'POST', JSON.stringify({participantId: contributor}))
-        console.log()
+        let response = await getResponse(`http://localhost:8080/users/${user.id}/projects/${project.id}/add-member`, 'POST', JSON.stringify({participantId: parseInt(contributor)}))
+        console.log(JSON.stringify({participantId: parseInt(contributor)}))
       }
   
       alert("Project created successfully.");
